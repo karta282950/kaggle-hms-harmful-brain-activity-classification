cf8c449d6e51d9bbf6050555941ab374b3778821
karta282950 <104354020@nccu.edu.tw>
2024-02-15 01:05:56 +0800

    修復load_from_checkpoint

--- inference.py
+++ inference.py
@@ -144,7 +144,7 @@ class InferenceConfig:
 
 
 @hydra.main(config_path="./", config_name="config", version_base="1.1")
-def main(cfg):
+def main(cfg: dict):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     TARGETS = ['seizure_vote', 'lpd_vote', 'gpd_vote', 'lrda_vote', 'grda_vote', 'other_vote']
     test = get_test_df(cfg)
@@ -154,8 +154,7 @@ def main(cfg):
     test_ds = CustomDataset(test, cfg=cfg, mode='test', specs=spectrograms, eeg_specs=all_eegs)
     test_loader = DataLoader(test_ds, shuffle=False, batch_size=64, num_workers=3)
     ckpt_file = cfg.LOAD_MODELS_FROM
-    model = CustomModel(cfg)
-    model.load_from_checkpoint(ckpt_file)
+    model = CustomModel.load_from_checkpoint(ckpt_file, cfg)
     
     model.to(device).eval()
     preds = []
