5f45c79a253301a471bada97d85fef3b4dc8b113
karta282950 <104354020@nccu.edu.tw>
2024-02-14 23:49:03 +0800

    修復load_from_checkpoint的cfg

--- inference.py
+++ inference.py
@@ -139,7 +139,7 @@ def main(cfg):
     test_ds = CustomDataset(test, cfg=cfg, mode='test', specs=spectrograms, eeg_specs=all_eegs)
     test_loader = DataLoader(test_ds, shuffle=False, batch_size=64, num_workers=3)
     ckpt_file = cfg.LOAD_MODELS_FROM
-    model = CustomModel(cfg).load_from_checkpoint(ckpt_file)
+    model = CustomModel.load_from_checkpoint(ckpt_file, cfg)
     model.to(device).eval()
     preds = []
     with torch.inference_mode():
