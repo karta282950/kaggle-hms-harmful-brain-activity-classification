4244341ce09905bfa2dad03e246230dc192d0ac6
karta282950 <104354020@nccu.edu.tw>
2024-02-23 09:21:02 +0800

    去除所有自定義progress bar

--- train.py
+++ train.py
@@ -47,125 +47,6 @@ def seed_everything(seed):
     np.random.seed(seed)#numpy的随机种子
     random.seed(seed)#python内置的随机种子
 
-#測試val進度條
-class MyProgressBar(TQDMProgressBar):
-    def init_validation_tqdm(self):
-        bar = super().init_validation_tqdm()
-        if not sys.stdout.isatty():
-            bar.disable = True
-        return bar
-
-    def init_predict_tqdm(self):
-        bar = super().init_predict_tqdm()
-        if not sys.stdout.isatty():
-            bar.disable = True
-        return bar
-
-    def init_test_tqdm(self):
-        bar = super().init_test_tqdm()
-        if not sys.stdout.isatty():
-            bar.disable = True
-        return bar
-
-class LitProgressBar(TQDMProgressBar):
-    def init_validation_tqdm(self):
-        bar = super().init_validation_tqdm()
-        bar.set_description("running validation...")
-        return bar
-
-from pytorch_lightning.callbacks import Callback
-class FlexibleTqdm(Callback):
-    def __init__(self, steps, column_width=10):
-        super(FlexibleTqdm, self).__init__()
-        self.steps = steps
-        self.column_width = column_width
-        self.info = "\rEpoch_%d %s%% [%s]"
-
-    def on_train_start(self, trainer, module):
-        history = module.history
-        self.row = "-" * (self.column_width + 1) * (len(history) + 2) + "-"
-        title = "|"
-        title += "epoch".center(self.column_width) + "|"
-        title += "time".center(self.column_width) + "|"
-        for i in history.keys():
-            title += i.center(self.column_width) + "|"
-        print(self.row)
-        print(title)
-        print(self.row)
-    
-    def on_train_batch_end(self, trainer, module, outputs, batch, batch_idx):
-        current_index = int((batch_idx + 1) * 100 / self.steps)
-        tqdm = ["."] * 100
-        for i in range(current_index - 1):
-            tqdm[i] = "="
-        if current_index:
-            tqdm[current_index - 1] = ">"
-        print(self.info % (module.current_epoch, str(current_index).rjust(3), "".join(tqdm)), end="")
-    '''
-    def on_train_epoch_start(self, trainer, module):
-        print(self.info % (module.current_epoch, "  0", "." * 100), end="")
-        self.begin = time.perf_counter()
-    
-    def on_train_epoch_end(self, trainer, module):
-        self.end = time.perf_counter()
-        history = module.history
-        print(history)
-        detail = "\r|"
-        detail += str(module.current_epoch).center(self.column_width) + "|"
-        detail += ("%d" % (self.end - self.begin)).center(self.column_width) + "|"
-        print(history)
-        for j in history.values():
-            #value = j[-1] if j else 0
-            detail += ("%.06f" % j[-1]).center(self.column_width) + "|"
-        print("\r" + " " * 120, end="")
-        print(detail)
-        print(self.row)
-    '''
-class LitProgressBar(ProgressBar):
-
-    def init_train_tqdm(self) -> tqdm:
-        """ Override this to customize the tqdm bar for training. """
-        bar = tqdm(
-            desc='Training',
-            initial=self.train_batch_idx,
-            position=(2 * self.process_position),
-            disable=self.is_disabled,
-            leave=True,
-            dynamic_ncols=False,  # This two lines are only for pycharm
-            ncols=100,
-            file=sys.stdout,
-            smoothing=0,
-        )
-        return bar
-
-    def init_validation_tqdm(self) -> tqdm:
-        """ Override this to customize the tqdm bar for validation. """
-        # The main progress bar doesn't exist in `trainer.validate()`
-        has_main_bar = self.main_progress_bar is not None
-        bar = tqdm(
-            desc='Validating',
-            position=(2 * self.process_position + has_main_bar),
-            disable=self.is_disabled,
-            leave=False,
-            dynamic_ncols=False,
-            ncols=100,
-            file=sys.stdout
-        )
-        return bar
-
-    def init_test_tqdm(self) -> tqdm:
-        """ Override this to customize the tqdm bar for testing. """
-        bar = tqdm(
-            desc="Testing",
-            position=(2 * self.process_position),
-            disable=self.is_disabled,
-            leave=True,
-            dynamic_ncols=False,
-            ncols=100,
-            file=sys.stdout
-        )
-        return bar
-
 @hydra.main(config_path="./", config_name="config", version_base="1.1")
 def main(cfg):
     seed_everything(cfg.SEED)
@@ -206,7 +87,7 @@ def main(cfg):
         max_steps=cfg.EPOCHS * len(datamodule.train_dataloader()),
         gradient_clip_val=cfg.gradient_clip_val,
         accumulate_grad_batches=cfg.accumulate_grad_batches,
-        callbacks=[checkpoint_cb, lr_monitor, model_summary, early_stopping, FlexibleTqdm(len(datamodule.train_ds) // 32, column_width=12)],
+        callbacks=[checkpoint_cb, lr_monitor, model_summary, early_stopping],
         logger=pl_logger,
         # resume_from_checkpoint=resume_from,
         num_sanity_val_steps=0,
